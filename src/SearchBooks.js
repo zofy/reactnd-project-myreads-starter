f8c984c6482e019c30c20e7506e250b8db6869a1
Patrik Puchala <patrik.puchala@exponea.com>
2021-01-03 19:59:34 +0100

    Fix invalid HTML
    It is invalid to have a <button> tag inside of amchor tag. For this
    reason, Link tag was modified and button removed.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -37,7 +37,7 @@ class SearchBooks extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link to="/"><button className="close-search">Close</button></Link>
+                    <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
                         <input
                             type="text"
