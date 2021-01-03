e77e54de43a786d1b98529c28177bce825ee0f53
Patrik Puchala <patrik.puchala@exponea.com>
2021-01-03 15:53:35 +0100

    Create SearchBooks component
    This component is responsible for providing a functionality for
    searching a book or set of books based on a given query. It is expected
    to be called with the `shelves` (BooksApp.categories) and
    `onShelfUpdate` as it should be possible to change the shelf of the book
    on the search page too.

---
+++ src/SearchBooks.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom';
+import ListBooks from './ListBooks';
+
+class SearchBooks extends Component {
+    state = {
+        query: "",
+        searchResult: [],
+    }
+
+    searchBooks = (query) => {
+        if (query === "") {
+            this.setState(() => ({searchResult: []}));
+            return;
+        }
+        BooksAPI.search(query)
+            .then((books) => {
+                if (this.state.query === query) {
+                    this.setState(() => ({
+                        searchResult: (books === undefined || books.error) ? [] : books,
+                    }));
+                }
+            });
+    }
+
+    updateQuery = (text) => {
+        this.setState(() => ({
+            query: text,
+        }));
+        this.searchBooks(text);
+    }
+
+    render () {
+        const { query, searchResult } = this.state;
+        const { shelves, onShelfUpdate } = this.props;
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/"><button className="close-search">Close</button></Link>
+                    <div className="search-books-input-wrapper">
+                        <input
+                            type="text"
+                            value={query}
+                            placeholder="Search by title or author"
+                            onChange={(e) => {this.updateQuery(e.target.value)}}
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ListBooks
+                        books={searchResult}
+                        shelves={shelves}
+                        onShelfUpdate={onShelfUpdate}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBooks;
